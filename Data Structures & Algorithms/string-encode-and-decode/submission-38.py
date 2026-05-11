09bdaa9888fe5d9cf089bacc332f9e83f9379b74
Carlos Aguilera <58356571+devloos@users.noreply.github.com>
2026-05-10 23:06:01 -0700

    Add: string-encode-and-decode - submission-38

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-38.py
@@ -0,0 +1,41 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        s = ''
+
+        for word in strs:
+            s = s + '#' + f"{len(word)}" + '#' + word
+            
+        return s
+
+        
+
+    def decode(self, s: str) -> List[str]:
+        result = []
+
+        i = 0
+
+        while i < len(s):
+            if s[i] == '#':
+                i += 1 # currently we see # so increase to get number
+                letter_count = ''
+
+                while s[i] != '#':
+                    letter_count += s[i]
+                    i += 1
+                
+                letter_count = int(letter_count)
+
+                i += 1
+                word = ""
+
+                j = i + letter_count
+
+                while i < len(s) and i < j:
+                    word += s[i]
+                    i += 1
+                
+                result.append(word)
+            
+
+        return result
